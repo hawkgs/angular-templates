019918fa176b929fe99233f5547274a12765258a
hawkgs <hawk.dvp@gmail.com>
2024-02-21 15:04:52 +0200

    chore: setup eslint and prettier

--- projects/ecommerce/src/app/app.component.ts
+++ projects/ecommerce/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ec-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ecommerce';
