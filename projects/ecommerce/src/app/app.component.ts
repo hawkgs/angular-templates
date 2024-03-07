7023a35387e0192a831f3f3bb49644e73c3e776e
hawkgs <hawk.dvp@gmail.com>
2024-03-07 17:24:48 +0200

    refactor(ecommerce): move app component providers to the app config

--- projects/ecommerce/src/app/app.component.ts
+++ projects/ecommerce/src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductsService } from './state/products.service';
-import { fetchMockApiProvider } from './shared/fetch';
-import { ProductsApi } from './api/products-api.service';
-import { CategoriesApi } from './api/categories-api.service';
 import { CategoriesService } from './state/categories.service';
 
 @Component({
   selector: 'ec-root',
   standalone: true,
   imports: [RouterOutlet],
-  // Substitute "fetchMockApiProvider" with "fetchApiProvider"
-  // in order to perform actual network requests via the Fetch API
-  providers: [
-    fetchMockApiProvider,
-    CategoriesApi,
-    ProductsApi,
-    CategoriesService,
-    ProductsService,
-  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- projects/ecommerce/src/app/app.config.ts
+++ projects/ecommerce/src/app/app.config.ts
@@ -1,9 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { APP_ROUTES } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { APP_ROUTES } from './app.routes';
+import { fetchMockApiProvider } from './shared/fetch';
+import { CategoriesApi } from './api/categories-api.service';
+import { ProductsApi } from './api/products-api.service';
+import { CategoriesService } from './state/categories.service';
+import { ProductsService } from './state/products.service';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(APP_ROUTES), provideClientHydration()],
+  providers: [
+    provideRouter(APP_ROUTES),
+    provideClientHydration(),
+    // Substitute "fetchMockApiProvider" with "fetchApiProvider"
+    // in order to perform actual network requests via the Fetch API
+    fetchMockApiProvider,
+    CategoriesApi,
+    ProductsApi,
+    CategoriesService,
+    ProductsService,
+  ],
 };
