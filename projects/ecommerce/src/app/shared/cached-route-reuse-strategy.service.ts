60348ad6c84313f73cf5cf5840a0d141ec3e06f5
hawkgs <hawk.dvp@gmail.com>
2024-03-12 17:19:28 +0200

    fix(ecommerce): cached route reuse strategy

--- projects/ecommerce/src/app/shared/cached-route-reuse-strategy.service.ts
+++ projects/ecommerce/src/app/shared/cached-route-reuse-strategy.service.ts
@@ -6,10 +6,7 @@ import {
 } from '@angular/router';
 
 const getReuseFromRoutes = (route: ActivatedRouteSnapshot): string[] =>
-  route.routeConfig?.data ? route.routeConfig.data['reuseFrom'] : [];
-
-const isReusable = (route: ActivatedRouteSnapshot): boolean =>
-  !!getReuseFromRoutes(route).length;
+  route.routeConfig?.data ? route.routeConfig.data['reuseFrom'] || [] : [];
 
 const getRoutePath = (route: ActivatedRouteSnapshot): string =>
   route.pathFromRoot
@@ -18,17 +15,28 @@ const getRoutePath = (route: ActivatedRouteSnapshot): string =>
     .filter((s) => !!s)
     .join('/');
 
+// The store key is represented by the route path.
 const getStoreKey = (route: ActivatedRouteSnapshot) => getRoutePath(route);
 
+// This custom strategy allows us to reuse/cache specific pages (components)
+// when navigating away from them by providing a "reuseFrom" array to your
+// route config path. "reuseFrom" paths tell the strategy to reuse the
+// respective route (i.e. component) when navigating from any of the listed
+// route paths in the array to the target route. Of course, the route will
+// be reused only if it has been previously loaded in the cache.
+//
+// More details about the interface:
+// https://github.com/angular/angular/blob/main/packages/router/src/route_reuse_strategy.ts#L80
 @Injectable()
 export class CachedRouteReuseStrategy implements RouteReuseStrategy {
-  private _cached: Map<string, DetachedRouteHandle> = new Map();
-  private _lastRoutePath: string = '';
+  private _store: Map<string, DetachedRouteHandle> = new Map();
+  private _newRoutePath: string = '';
 
   shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    this._lastRoutePath = getRoutePath(route);
+    const reuseFromRoutes = getReuseFromRoutes(route);
+    const shouldReuse = reuseFromRoutes.includes(this._newRoutePath);
 
-    return isReusable(route);
+    return shouldReuse;
   }
 
   store(
@@ -38,21 +46,20 @@ export class CachedRouteReuseStrategy implements RouteReuseStrategy {
     const key = getStoreKey(route);
 
     if (handle) {
-      this._cached.set(key, handle);
+      this._store.set(key, handle);
     } else {
-      this._cached.delete(key);
+      this._store.delete(key);
     }
   }
 
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    const reuseFromRoutes = getReuseFromRoutes(route);
-    const shouldReuse = reuseFromRoutes.includes(this._lastRoutePath);
+    this._newRoutePath = getRoutePath(route);
 
-    return shouldReuse && this._cached.has(getStoreKey(route));
+    return this._store.has(getStoreKey(route));
   }
 
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
-    return this._cached.get(getStoreKey(route)) || null;
+    return this._store.get(getStoreKey(route)) || null;
   }
 
   shouldReuseRoute(
