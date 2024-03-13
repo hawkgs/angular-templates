dbfdbea46b66a0f9b93ca111993db1c7f104eb03
hawkgs <hawk.dvp@gmail.com>
2024-03-13 17:53:18 +0200

    feat(ecommerce): save cart state to local storage

--- projects/ecommerce/src/app/data-access/cart.service.ts
+++ projects/ecommerce/src/app/data-access/cart.service.ts
@@ -1,20 +1,85 @@
-import { signal } from '@angular/core';
+import {
+  AfterRenderPhase,
+  afterNextRender,
+  effect,
+  inject,
+  signal,
+  untracked,
+} from '@angular/core';
 import { Map } from 'immutable';
 import { Product } from '../../models';
+import { LocalStorage } from '../shared/local-storage.service';
+
+const CART_LS_KEY = 'ec-cart';
 
 /**
  * Cart state.
  */
 export class CartService {
+  private _storage = inject(LocalStorage);
+
   private _cart = signal<Map<string, number>>(Map([]));
+  private _products = signal<Map<string, Product>>(Map([]));
+
   readonly value = this._cart.asReadonly();
 
+  constructor() {
+    // _loadCartInState uses browser API; hence, we need
+    // ot defer the execution after the SSR render.
+    afterNextRender(() => this._loadCartInState(), {
+      phase: AfterRenderPhase.Read,
+    });
+
+    // Any update of the _cart signal after the _loadCartInState
+    // will result in a Storage update.
+    effect(() => {
+      this._storage.setJSON(CART_LS_KEY, this._cart().toJSON());
+    });
+  }
+
   addToCart(product: Product, quantity: number = 1) {
-    const currQuantity = this._cart().get(product.id) || 0;
-    this._cart.update((m) => m.set(product.id, currQuantity + quantity));
+    this.updateQuantity(product, quantity);
+  }
+
+  updateQuantity(
+    product: Product,
+    quantity: number = 1,
+    relativeQuantity: boolean = true,
+  ) {
+    const currQuantity = relativeQuantity
+      ? this._cart().get(product.id) || 0
+      : 0;
+    const nextQuantity = currQuantity + quantity;
+
+    if (!nextQuantity) {
+      this.removeFromCart(product);
+      return;
+    }
+
+    this._cart.update((m) => m.set(product.id, nextQuantity));
+
+    if (!this._products().has(product.id)) {
+      this._products.update((m) => m.set(product.id, product));
+    }
   }
 
   removeFromCart(product: Product) {
     this._cart.update((m) => m.delete(product.id));
+    this._products.update((m) => m.delete(product.id));
+  }
+
+  private _loadCartInState() {
+    const cart = this._storage.getJSON(CART_LS_KEY) as {
+      [key: string]: number;
+    } | null;
+
+    if (cart) {
+      for (const productId in cart) {
+        const quantity = cart[productId];
+        untracked(() => {
+          this._cart.update((m) => m.set(productId, quantity));
+        });
+      }
+    }
   }
 }
--- projects/ecommerce/src/app/shared/local-storage.service.ts
+++ projects/ecommerce/src/app/shared/local-storage.service.ts
@@ -1,41 +1,44 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Injectable, PLATFORM_ID, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 /**
  * localStorage wrapper. Browser-only.
  * Should not be used during the server-side
  * rendering phase (no use case).
  */
+// We intentionally don't check if the platform is browser.
+// Otherwise, we won't get an error on the server, if we
+// happen to use LS during the render phase. As a result,
+// any problems caused by that might go unnoticed or hard
+// to track.
 @Injectable({ providedIn: 'root' })
 export class LocalStorage {
-  private _platformId = inject(PLATFORM_ID);
-
-  private get _isBrowser() {
-    return isPlatformBrowser(this._platformId);
-  }
-
   get(key: string): string | null {
-    if (!this._isBrowser) {
-      return null;
-    }
     return localStorage.getItem(key);
   }
 
-  set(key: string, data: string) {
-    if (this._isBrowser) {
-      localStorage.setItem(key, data);
+  getJSON(key: string): object | null {
+    const item = this.get(key);
+
+    try {
+      return JSON.parse(item || '');
+    } catch {
+      return null;
     }
+  }
+
+  set(key: string, data: string) {
+    localStorage.setItem(key, data);
+  }
+
+  setJSON(key: string, data: object) {
+    localStorage.setItem(key, JSON.stringify(data));
   }
 
   remove(key: string) {
-    if (this._isBrowser) {
-      localStorage.removeItem(key);
-    }
+    localStorage.removeItem(key);
   }
 
   clear() {
-    if (this._isBrowser) {
-      localStorage.clear();
-    }
+    localStorage.clear();
   }
 }
