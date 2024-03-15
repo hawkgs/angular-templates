d909c004fc834c4cd5610a5ee6080c304a7f0c5a
hawkgs <hawk.dvp@gmail.com>
2024-03-15 15:45:16 +0200

    feat(ecommerce): style header

--- projects/ecommerce/src/app/shared/header/header.component.ts
+++ projects/ecommerce/src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../data-access/cart.service';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'ec-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, IconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
