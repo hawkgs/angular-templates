f0c793744f9262b5e8c68587029b6ef13ea4852a
hawkgs <hawk.dvp@gmail.com>
2024-05-08 13:42:37 +0300

    feat(dashboard): implement anchoring for the draggable directive

--- projects/dashboard/src/app/shared/draggable/draggable.directive.ts
+++ projects/dashboard/src/app/shared/draggable/draggable.directive.ts
@@ -12,6 +12,8 @@ import {
 
 type Coor = { x: number; y: number };
 
+const RAPPEL_ANIM_DURR = 300;
+
 @Directive({
   selector: '[dbDraggable]',
   standalone: true,
@@ -31,18 +33,17 @@ export class DraggableDirective implements OnInit, OnDestroy {
   @Input('dbDraggable')
   set active(v: boolean) {
     this._active = v;
+  }
 
-    if (this._active) {
-      // Styles should be applied upon drag start
-      this._applyDraggableStyles();
-    }
+  private get _el(): HTMLElement {
+    return this._elRef.nativeElement;
   }
 
   ngOnInit(): void {
     this._zone.runOutsideAngular(() => {
       this._listeners = [
         this._renderer.listen(
-          this._elRef.nativeElement,
+          this._el,
           'mousedown',
           this._onDragStart.bind(this),
         ),
@@ -64,17 +65,17 @@ export class DraggableDirective implements OnInit, OnDestroy {
     if (!this._active) {
       return;
     }
+
     this._dragging = true;
 
-    const { x, y } = this._elRef.nativeElement.getBoundingClientRect();
+    const { x, y } = this._el.getBoundingClientRect();
 
-    if (!this._initPos) {
-      this._initPos = { x, y };
-    }
+    this._initPos = { x, y };
     this._relativeMousePos = {
       x: e.clientX - x,
       y: e.clientY - y,
     };
+    this._applyDraggableStyles(this._initPos);
   }
 
   private _onDragMove(e: MouseEvent) {
@@ -85,23 +86,58 @@ export class DraggableDirective implements OnInit, OnDestroy {
   }
 
   private _onDragEnd() {
-    this._dragging = false;
+    if (this._dragging) {
+      this._moveToAnchorPos();
+      this._dragging = false;
+    }
   }
 
-  private _applyDraggableStyles() {
-    this._renderer.setStyle(this._elRef.nativeElement, 'position', 'absolute');
+  private _applyDraggableStyles(initPos: Coor) {
+    this._setStyles({
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      opacity: '0.6',
+    });
+    this._move(initPos, false);
   }
 
-  private _move(coor: Coor) {
-    const ip = this._initPos!;
-    const rmp = this._relativeMousePos;
-    const offset = { x: ip.x + rmp.x, y: ip.y + rmp.y };
-    const translateStr = `translate(${coor.x - offset.x}px, ${coor.y - offset.y}px)`;
+  private _move(coor: Coor, factorInMouse: boolean = true) {
+    const offset = factorInMouse ? this._relativeMousePos : { x: 0, y: 0 };
+    const translate = `translate(${coor.x - offset.x}px, ${coor.y - offset.y}px)`;
 
+    this._renderer.setStyle(this._el, 'transform', translate);
+  }
+
+  private _moveToAnchorPos() {
     this._renderer.setStyle(
-      this._elRef.nativeElement,
-      'transform',
-      translateStr,
+      this._el,
+      'transition',
+      `transform ${RAPPEL_ANIM_DURR}ms ease`,
     );
+
+    this._move(this._initPos!, false);
+
+    setTimeout(() => {
+      this._removeStyles([
+        'transition',
+        'position',
+        'top',
+        'left',
+        'opacity',
+        'transform',
+      ]);
+    }, RAPPEL_ANIM_DURR);
+  }
+
+  private _setStyles(stylesObj: { [key: string]: string }) {
+    for (const cssProp in stylesObj) {
+      const value = stylesObj[cssProp];
+      this._renderer.setStyle(this._el, cssProp, value);
+    }
+  }
+
+  private _removeStyles(cssProps: string[]) {
+    cssProps.forEach((p) => this._renderer.removeStyle(this._el, p));
   }
 }
