68f18fdad9fd4fc70416d3015eca7ce9682fd40b
hawkgs <hawk.dvp@gmail.com>
2024-05-22 19:18:11 +0300

    feat(dashboard): improve styling

--- projects/shared/button/src/button.component.ts
+++ projects/shared/button/src/button.component.ts
@@ -1,6 +1,14 @@
 /* eslint-disable @angular-eslint/no-host-metadata-property */
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, HostBinding, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  OnInit,
+  Renderer2,
+  inject,
+  input,
+} from '@angular/core';
 
 type ButtonType = 'primary' | 'secondary';
 
@@ -14,22 +22,16 @@ type ButtonType = 'primary' | 'secondary';
     class: 'ngx-button',
   },
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
+  private _renderer = inject(Renderer2);
+  private _elRef = inject(ElementRef);
+
   btnType = input.required<ButtonType>();
-  size = input<'compact' | 'large'>('compact');
+  size = input<'compact' | 'large' | 'minimal'>('compact');
 
-  @HostBinding('class.large-size-btn')
-  get isLarge() {
-    return this.size() === 'large';
-  }
-
-  @HostBinding('class.primary-btn')
-  get isPrimary() {
-    return this.btnType() === 'primary';
-  }
-
-  @HostBinding('class.secondary-btn')
-  get isSecondary() {
-    return this.btnType() === 'secondary';
+  ngOnInit() {
+    const el = this._elRef.nativeElement;
+    this._renderer.addClass(el, `${this.btnType()}-btn`);
+    this._renderer.addClass(el, `${this.size()}-size-btn`);
   }
 }
