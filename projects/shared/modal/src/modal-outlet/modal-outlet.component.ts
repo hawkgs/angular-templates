245c6511c0eeebad262496cbb7083a019fcaabe1
hawkgs <hawk.dvp@gmail.com>
2024-06-18 11:24:48 +0300

    feat(shared): block page scrolling while there is an opened modal

--- projects/shared/modal/src/modal-outlet/modal-outlet.component.ts
+++ projects/shared/modal/src/modal-outlet/modal-outlet.component.ts
@@ -1,6 +1,13 @@
-import { Component, HostListener, inject } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Renderer2,
+  effect,
+  inject,
+} from '@angular/core';
 import { ModalService } from '../modal.service';
 import { ModalComponent } from '../modal/modal.component';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'ngx-modal-outlet',
@@ -10,8 +17,23 @@ import { ModalComponent } from '../modal/modal.component';
   styleUrl: './modal-outlet.component.css',
 })
 export class ModalOutletComponent {
+  private _doc = inject(DOCUMENT);
   private _modalService = inject(ModalService);
+  private _renderer = inject(Renderer2);
+
   modals = this._modalService.modals;
+
+  constructor() {
+    effect(() => {
+      const body = this._doc.body;
+
+      if (this.modals().size) {
+        this._renderer.setStyle(body, 'overflow', 'hidden');
+      } else {
+        this._renderer.setStyle(body, 'overflow', null);
+      }
+    });
+  }
 
   @HostListener('document:keydown.escape')
   closeCurrentVisibleModal() {
