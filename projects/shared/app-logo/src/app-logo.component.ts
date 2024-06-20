4e71dc04ebcf6f72bebab0a826ecde09dbf216b9
hawkgs <hawk.dvp@gmail.com>
2024-06-20 11:00:52 +0300

    feat(dashboard): drop routing

--- projects/shared/app-logo/src/app-logo.component.ts
+++ projects/shared/app-logo/src/app-logo.component.ts
@@ -1,12 +1,14 @@
-import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ngx-app-logo',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app-logo.component.html',
-  styleUrl: './app-logo.component.css',
+  styleUrl: './app-logo.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppLogoComponent {
   text = input.required<string>();
