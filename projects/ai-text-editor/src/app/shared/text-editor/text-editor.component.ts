0a2b246a780b52fe862034c8c2ffe61731b7284e
hawkgs <hawk.dvp@gmail.com>
2024-07-19 18:19:00 +0300

    feat(ai-text-editor): auto focus editor on page load

--- projects/ai-text-editor/src/app/shared/text-editor/text-editor.component.ts
+++ projects/ai-text-editor/src/app/shared/text-editor/text-editor.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
 
 @Component({
   selector: 'ate-text-editor',
   standalone: true,
   imports: [],
   templateUrl: './text-editor.component.html',
-  styleUrl: './text-editor.component.scss'
+  styleUrl: './text-editor.component.scss',
 })
-export class TextEditorComponent {
+export class TextEditorComponent implements AfterViewInit {
+  editor = viewChild.required<ElementRef>('editor');
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.editor().nativeElement.focus();
+    });
+  }
 }
