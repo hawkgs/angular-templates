9173560d71aa08e5d5929ca54d01e77a377e0e57
hawkgs <hawk.dvp@gmail.com>
2024-07-19 19:07:59 +0300

    feat(ai-text-editor): implement alpha ver of the text formatting functionality

--- projects/ai-text-editor/src/app/app.config.ts
+++ projects/ai-text-editor/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-
 import { provideClientHydration } from '@angular/platform-browser';
+import { windowProvider } from '@ngx-templates/shared/services';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideClientHydration(),
+    windowProvider,
   ],
 };
--- projects/ai-text-editor/src/app/shared/text-editor/text-editor.component.ts
+++ projects/ai-text-editor/src/app/shared/text-editor/text-editor.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  viewChild,
+} from '@angular/core';
+import { WINDOW } from '@ngx-templates/shared/services';
 
 @Component({
   selector: 'ate-text-editor',
@@ -8,11 +16,76 @@ import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
   styleUrl: './text-editor.component.scss',
 })
 export class TextEditorComponent implements AfterViewInit {
+  private _win = inject(WINDOW);
+  private _doc = inject(DOCUMENT);
+
   editor = viewChild.required<ElementRef>('editor');
 
   ngAfterViewInit() {
     setTimeout(() => {
       this.editor().nativeElement.focus();
     });
+  }
+
+  bold() {
+    this._formatSelection(
+      () => this._doc.createElement('b'),
+      (el: HTMLElement) => el.tagName === 'B',
+    );
+  }
+
+  italics() {
+    this._formatSelection(
+      () => this._doc.createElement('em'),
+      (el: HTMLElement) => el.tagName === 'EM',
+    );
+  }
+
+  // Note(Georgi): Not finalized
+  private _formatSelection(
+    formattedElementFactory: () => HTMLElement,
+    formattedElementTest: (el: HTMLElement) => boolean,
+  ) {
+    const selection = this._win.getSelection();
+    const range = selection?.getRangeAt(0);
+
+    if (!selection || !range) {
+      return;
+    }
+
+    const text = selection.toString();
+
+    const { startContainer, endContainer } = range;
+    const isSameParent =
+      startContainer.parentElement === endContainer.parentElement;
+    const isSameData = startContainer.textContent === endContainer.textContent;
+    const isFullOffset =
+      range.startOffset === 0 && range.endOffset === text.length;
+    const isParentElementFormatted = formattedElementTest(
+      startContainer.parentElement!,
+    );
+    const isFormatted =
+      isSameParent && isSameData && isFullOffset && isParentElementFormatted;
+
+    console.log(
+      isSameParent,
+      isSameData,
+      isFullOffset,
+      isParentElementFormatted,
+    );
+
+    let newNode: Node;
+
+    if (!isFormatted) {
+      const formattedEl = formattedElementFactory();
+      formattedEl.innerText = text;
+      newNode = formattedEl;
+    } else {
+      newNode = this._doc.createTextNode(text);
+      startContainer.parentElement?.remove();
+    }
+
+    range.deleteContents();
+    range.insertNode(newNode);
   }
 }
