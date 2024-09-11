0c084101e190ab51845ddc80e2488afed437ce98
hawkgs <hawk.dvp@gmail.com>
2024-09-11 17:03:11 +0300

    feat(kanban): initial API-state management integration

--- projects/kanban/src/app/board/shared/card/card.component.ts
+++ projects/kanban/src/app/board/shared/card/card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Card } from '../../../../models';
 
 @Component({
   selector: 'kb-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  card = input.required<Card>();
 }
