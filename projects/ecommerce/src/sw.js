864299ac0e3a360255b4490e387480434c7f3463
hawkgs <hawk.dvp@gmail.com>
2024-09-17 17:47:48 +0300

    feat(ecommerce): add no-cache header to the requests in the SW

--- projects/ecommerce/src/sw.js
+++ projects/ecommerce/src/sw.js
@@ -1,9 +1,22 @@
-const wait = (time) =>
-  new Promise((resolve) => {
+// Keeps the currently set fetch delay
+let delay = 0;
+
+function wait(time) {
+  return new Promise((resolve) => {
     setTimeout(() => resolve(), time);
   });
+}
 
-let delay = 0;
+function addCacheControlHeader(event) {
+  const headers = new Headers(event.request.headers);
+  headers.set('Cache-Control', 'no-cache');
+
+  return new Request(event.request, {
+    mode: 'cors',
+    credentials: 'omit',
+    headers: headers,
+  });
+}
 
 self.addEventListener('message', (event) => {
   console.log('Delay updated to:', delay);
@@ -12,6 +25,7 @@ self.addEventListener('message', (event) => {
 
 self.addEventListener('fetch', function (event) {
   console.log('Fetch delay at:', delay);
-  const fetchPromise = wait(delay).then(() => fetch(event.request));
+  const noCacheRequest = addCacheControlHeader(event);
+  const fetchPromise = wait(delay).then(() => fetch(noCacheRequest));
   event.respondWith(fetchPromise);
 });
