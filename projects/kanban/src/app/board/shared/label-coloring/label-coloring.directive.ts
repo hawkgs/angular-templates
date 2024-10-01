bbe18a13ce08bf953d81d46ca7c5d774f376052b
hawkgs <hawk.dvp@gmail.com>
2024-10-01 20:39:41 +0300

    fix(kanban): make label coloring directive reactive

--- projects/kanban/src/app/board/shared/label-coloring/label-coloring.directive.ts
+++ projects/kanban/src/app/board/shared/label-coloring/label-coloring.directive.ts
@@ -1,11 +1,4 @@
-import {
-  Directive,
-  ElementRef,
-  inject,
-  input,
-  OnInit,
-  Renderer2,
-} from '@angular/core';
+import { Directive, effect, ElementRef, input, Renderer2 } from '@angular/core';
 import { Label } from '../../../../models';
 import { LABEL_COLORS } from './label-colors';
 
@@ -16,21 +9,18 @@ const GREY_50 = '#fbfbfb';
   selector: '[kbLabelColoring]',
   standalone: true,
 })
-export class LabelColoringDirective implements OnInit {
-  private _elRef = inject(ElementRef);
-  private _renderer = inject(Renderer2);
+export class LabelColoringDirective {
   label = input.required<Label>({ alias: 'kbLabelColoring' });
 
   constructor({ nativeElement }: ElementRef, renderer: Renderer2) {
-    renderer.setStyle(nativeElement, 'background-color', LABEL_COLORS);
-  }
+    effect(() => {
+      const el = nativeElement;
+      const color = LABEL_COLORS.get(this.label().color);
+      const text = color?.light ? GREY_900 : GREY_50;
 
-  ngOnInit() {
-    const el = this._elRef.nativeElement;
-    const color = LABEL_COLORS.get(this.label().color);
-    const text = color?.light ? GREY_900 : GREY_50;
-
-    this._renderer.setStyle(el, 'background-color', color?.hex);
-    this._renderer.setStyle(el, 'color', text);
+      renderer.setStyle(el, 'background-color', color?.hex);
+      renderer.setStyle(el, 'color', text);
+      renderer.setStyle(el, 'user-select', 'none');
+    });
   }
 }
