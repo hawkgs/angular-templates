4e482704bc8ebf2ad9e935eadb55ffa674cae29a
hawkgs <hawk.dvp@gmail.com>
2024-10-02 11:27:44 +0300

    feat(shared): add support for multiple text sizes in the modal content

--- projects/shared/modal/src/modal-content/modal-content.component.ts
+++ projects/shared/modal/src/modal-content/modal-content.component.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, HostBinding, input } from '@angular/core';
 import { IconComponent } from '@ngx-templates/shared/icon';
 import { ModalController } from '../modal.controller';
+
+type TextSize = 'standard' | 'compact';
 
 @Component({
   selector: 'ngx-modal-content',
@@ -11,4 +13,10 @@ import { ModalController } from '../modal.controller';
 })
 export class ModalContentComponent<T> {
   controller = input.required<ModalController<T>>();
+  textSize = input<TextSize>('standard');
+
+  @HostBinding('class.compact')
+  get isTextCompact() {
+    return this.textSize() === 'compact';
+  }
 }
