a22e2c58bc2ee91c4f729ac7420d202739b35cf8
hawkgs <hawk.dvp@gmail.com>
2024-10-08 17:00:33 +0300

    feat(ai-chatbot): create and setup project

---
+++ projects/ai-chatbot/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'acb-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
