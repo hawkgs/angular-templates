fe032f7133a739e0b7d8258dfc36d5b37d0a05a5
hawkgs <hawk.dvp@gmail.com>
2024-10-14 16:57:14 +0300

    feat(ai-chatbot): save sidebar expanded state in the LS

--- projects/ai-chatbot/src/app/shared/sidebar/sidebar.component.ts
+++ projects/ai-chatbot/src/app/shared/sidebar/sidebar.component.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 
 import { IconComponent } from '@ngx-templates/shared/icon';
 import { ThemeSwitchComponent } from '@ngx-templates/shared/theme';
+import { LocalStorage } from '@ngx-templates/shared/services';
 
 import { FooterComponent } from './footer/footer.component';
 import { ChatbotService } from '../../data-access/chatbot.service';
+
+const SIDEBAR_STATE_KEY = 'acb-sb-expanded';
 
 @Component({
   selector: 'acb-sidebar',
@@ -23,7 +26,20 @@ import { ChatbotService } from '../../data-access/chatbot.service';
 })
 export class SidebarComponent {
   chatbot = inject(ChatbotService);
+  private _storage = inject(LocalStorage);
+
   expanded = signal<boolean>(false);
+
+  constructor() {
+    const expanded = this._storage.get(SIDEBAR_STATE_KEY) === 'true';
+    this.expanded.set(expanded);
+  }
+
+  toggle() {
+    const expanded = !this.expanded();
+    this.expanded.set(expanded);
+    this._storage.set(SIDEBAR_STATE_KEY, expanded.toString());
+  }
 
   @HostBinding('class.expanded')
   get isExpanded() {
